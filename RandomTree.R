9e891378b5dc6a3d08b1059b67ea7a7153e1c3f8
klhscott <tianxiashilaofude@gmail.com>
2015-10-23 15:07:54 +0800

    Update RandomTree.R

--- RandomTree.R
+++ RandomTree.R
@@ -1,3 +1,4 @@
+# Preprocess
 data(iris)
 library(ggplot2)
 inTrain <- createDataPartition(y=iris$Species,p=0.7,list=FALSE)
@@ -7,7 +8,7 @@ testing <- iris[-inTrain,]
 library(caret)
 modelFit <- train(Species~.,data=training,method="rf",prox=TRUE)
 modelFit
-
+# Build Random Tree
 getTree(modelFit$finalModel, k=2)
 
 irisP <- classCenter(training[,c(3,4)],training$Species,modelFit$finalModel$prox)
@@ -15,3 +16,9 @@ irisP <- as.data.frame(irisP)
 irisP$Species <- rownames(irisP)
 p <- qplot(Petal.Width, Petal.Length, col=Species, data=training)
 p + geom_point(aes(x=Petal.Width,y=Petal.Length,col=Species),size=5,shape=4,data=irisP)
+# Predict testing data
+pred <- predict(modelFit,testing)
+testing$predRight <- pred==testing$Species
+table(pred,testing$Species)
+# Plot the test result
+qplot(Petal.Width,Petal.Length,colour=predRight,data=testing,main="newdata Preditcions")
