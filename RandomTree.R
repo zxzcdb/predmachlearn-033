a3cbed091c8f2d55f41c641424cf780bdad0821a
klhscott <tianxiashilaofude@gmail.com>
2015-10-23 15:00:38 +0800

    Update RandomTree.R

--- RandomTree.R
+++ RandomTree.R
@@ -9,3 +9,9 @@ modelFit <- train(Species~.,data=training,method="rf",prox=TRUE)
 modelFit
 
 getTree(modelFit$finalModel, k=2)
+
+irisP <- classCenter(training[,c(3,4)],training$Species,modelFit$finalModel$prox)
+irisP <- as.data.frame(irisP)
+irisP$Species <- rownames(irisP)
+p <- qplot(Petal.Width, Petal.Length, col=Species, data=training)
+p + geom_point(aes(x=Petal.Width,y=Petal.Length,col=Species),size=5,shape=4,data=irisP)
